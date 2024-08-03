0759a97c33eef4da04bc200992ae4a214b76359b
ChikatoTakenouchi <maxmaxastea@gmail.com>
2024-08-04 07:22:52 +0900

    make entity to public

--- Sources/WorldTimeAPIiOS/Response/CurrentTimeWithTimeZone.swift
+++ Sources/WorldTimeAPIiOS/Response/CurrentTimeWithTimeZone.swift
@@ -8,21 +8,36 @@
 import Foundation
 
 public struct CurrentTimeWithTimeZone: Decodable {
-    let abbreviation: String
-    let clientIP: String
-    let datetime: String
-    let dayOfWeek: Int
-    let dayOfYear: Int
-    let dst: Bool
-    let dstFrom: String?
-    let dstOffset: Int
-    let dstUntil: String?
-    let rawOffset: Int
-    let timezone: String
-    let unixtime: Int
-    let utcDatetime: String
-    let utcOffset: String
-    let weekNumber: Int
+    /// the abbreviated name of the timezone
+    public let abbreviation: String
+    /// the IP of the client making the request
+    public let clientIP: String
+    /// an ISO8601-valid string representing the current, local date/time
+    public let datetime: String
+    /// current day number of the week, where sunday is 0
+    public let dayOfWeek: Int
+    /// ordinal date of the current year
+    public let dayOfYear: Int
+    /// flag indicating whether the local time is in daylight savings
+    public let dst: Bool
+    /// an ISO8601-valid string representing the datetime when daylight savings started for this timezone
+    public let dstFrom: String?
+    /// the difference in seconds between the current local time and daylight saving time for the location
+    public let dstOffset: Int
+    /// an ISO8601-valid string representing the datetime when daylight savings will end for this timezone
+    public let dstUntil: String?
+    /// the difference in seconds between the current local time and the time in UTC, excluding any daylight saving difference (see dst_offset)
+    public let rawOffset: Int
+    /// timezone in Area/Location or Area/Location/Region format
+    public let timezone: String
+    /// number of seconds since the Epoch
+    public let unixtime: Int
+    /// an ISO8601-valid string representing the current date/time in UTC
+    public let utcDatetime: String
+    /// an ISO8601-valid string representing the offset from UTC
+    public let utcOffset: String
+    /// the current week number
+    public let weekNumber: Int
 
     enum CodingKeys: String, CodingKey {
         case abbreviation
